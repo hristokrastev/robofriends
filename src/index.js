27f22513fc049a17cfd7d119452bf0be59d0b10d
hristokrastev <swifty1009>
2018-12-30 13:34:12 +0200

    wrapping App with Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import  { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 import './index.css';
 // import App from './App';
 // import Hello from './Hello';
@@ -8,7 +11,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchRobots)
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
